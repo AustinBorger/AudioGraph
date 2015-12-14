ac051b8bc765bc45314ce9f39a799209b1f89fab
Austin Borger <aaborger@gmail.com>
2015-12-14 01:59:52 -0500

    More methods

--- AudioGraph/AudioGraph.h
+++ AudioGraph/AudioGraph.h
@@ -25,19 +25,48 @@
 #include <comdef.h>
 
 struct __declspec(uuid("b7fa0e54-41d7-4161-81d6-3036900cfc80")) IAudioGraphCallback : public IUnknown {
+	/* OnObjectFailure() is a callback mechanism for error reporting.  If something unexpected happens,
+	** or the library is operating out of its expected conditions, then this will be called with the
+	** HRESULT that was provided by the Windows API when the error occurred. Note that this must be implemented. */
+	virtual VOID STDMETHODCALLTYPE OnObjectFailure(LPCWSTR File, UINT Line, HRESULT hr) PURE;
 
+	virtual LPCWSTR STDMETHODCALLTYPE OnTransition(IAudioGraph* pAudioGraph, IAudioGraphNode* pNode) PURE;
+};
+
+struct __declspec(uuid("2a4bee1e-2d02-4f9c-bed9-eaedfb95331d")) IAudioGraphEdge : public IUnknown {
+	virtual LPCWSTR STDMETHODCALLTYPE GetID() PURE;
+
+	virtual VOID GetFrom(IAudioGraphNode** ppNode) PURE;
+
+	virtual VOID GetTo(IAudioGraphNode** ppNode) PURE;
+
+	virtual LPCWSTR GetTrigger() PURE;
+};
+
+struct __declspec(uuid("b8fd4cc2-4360-4701-bb1c-8715fd77d38e")) IAudioGraphNode : public IUnknown {
+	virtual LPCWSTR STDMETHODCALLTYPE GetID() PURE;
+
+	virtual LONG GetNumEdges() PURE;
+
+	virtual VOID EnumEdge(LONG EdgeNum, IAudioGraphEdge** ppEdge) PURE;
 };
 
 struct __declspec(uuid("b1f2bb1c-f1da-4f0a-ba3a-b7dbe2a7c824")) IAudioGraph : public IUnknown {
+	virtual VOID STDMETHODCALLTYPE CreateNode(LPCWSTR Style, IAudioGraphNode** ppNode) PURE;
 
+	virtual VOID STDMETHODCALLTYPE CreateEdge(LPCWSTR Style, IAudioGraphEdge** ppEdge) PURE;
+
+	virtual VOID STDMETHODCALLTYPE GetNodeByID(LPCWSTR ID, IAudioGraphNode** ppNode) PURE;
+
+	virtual VOID STDMETHODCALLTYPE GetEdgeByID(LPCWSTR ID, IAudioGraphEdge** ppEdge) PURE;
 };
 
-/* IAudioGraphFactory provides several APIs to create audio graphs.  It also defines the connection
+/* IAudioGraphFactory provides several APIs to create audio graphs.  It also provides the connection
 ** between the application and the Windows audio service. */
 struct __declspec(uuid("b824c4eb-5a50-4706-8c14-bcc2f207d6ee")) IAudioGraphFactory : public IUnknown {
 	virtual VOID STDMETHODCALLTYPE CreateAudioGraphFromFile(LPCWSTR Filename, IAudioGraph** ppAudioGraph) PURE;
 
-	virtual VOID STDMETHODCALLTYPE CreateBlankAudioGraph(IAudioGraph** ppAudioGraph) PURE;
+	virtual VOID STDMETHODCALLTYPE CreateAudioGraph(IAudioGraph** ppAudioGraph) PURE;
 };
 
 #ifndef _AUDIO_GRAPH_EXPORT_TAG
